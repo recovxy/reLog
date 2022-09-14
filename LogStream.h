3d1a46baf8f9038a33b5c3abce22213ad46ac458
recovxy <recovxy@gmail.com>
2022-09-15 00:17:21 +0800

    first commit

---
+++ LogStream.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+template<int SIZE>
+class LogBuffer
+{
+public:
+    LogBuffer() : 
+
+private:
+    std::string buffer_;
+
+};
+
+class LogStream
+{
+public:
+
+
+private:
+
+};
